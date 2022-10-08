4bbe90c6c3b650d93fe25cd013bf90ed71559b03
László Gondi <laszlo.zsolt.gondi@gmail.com>
2022-10-08 16:45:30 +0200

    Add function that generates a word to guess

--- app/src/main/java/com/example/akasztofa/MainActivity.java
+++ app/src/main/java/com/example/akasztofa/MainActivity.java
@@ -8,16 +8,25 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.Locale;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
+    //Element variables
     private Button minusButton;
     private Button plusButton;
     private Button guessButton;
     private TextView guessLetter;
     private TextView guessWord;
     private ImageView image;
+
+    //Variables
     private final String[] letterList = {"a", "b", "c", "d", "e", "f", "g","h", "i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+    private String[] usedLetterList = new String[26];
     private int index;
+    private String[] wordList = {"fire", "police", "iphone", "keyboard", "house", "monitor", "clock","nagger","dragon", "racing"};
+    private String word;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
                 }else{
                     index--;
                 }
-                guessLetter.setText(letterList[index]);
+                guessLetter.setText(letterList[index].toUpperCase());
             }
         });
         plusButton.setOnClickListener(new View.OnClickListener() {
@@ -43,9 +52,25 @@ public class MainActivity extends AppCompatActivity {
                 }else{
                     index++;
                 }
-                guessLetter.setText(letterList[index]);
+                guessLetter.setText(letterList[index].toUpperCase());
             }
         });
+        guessButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private String thinkOfWord(String[] list){
+        String underlines="";
+        Random rng = new Random();
+        word = list[rng.nextInt(9)];
+        for (int i = 0; i < list.length; i++) {
+            underlines+="_";
+        }
+        return underlines;
     }
 
     private void init(){
@@ -56,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
         guessWord = findViewById(R.id.guessWord);
         image = findViewById(R.id.imageBox);
         index = 0;
-        guessLetter.setText(letterList[index]);
+        guessLetter.setText(letterList[index].toUpperCase());
+        guessWord.setText(thinkOfWord(wordList));
     }
 }
